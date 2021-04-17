8202a03d02e848000f491305d3f798ca035788a4
Furong Tian <tianfurong@gmail.com>
2021-04-17 17:59:35 -0400

    fix App.js

--- travelwithlocal_react/src/App.js
+++ travelwithlocal_react/src/App.js
@@ -2,46 +2,36 @@ import SearchScreen from "./components/search-screen";
 import DetailsScreen from "./components/details-screen";
 import {BrowserRouter, Route} from "react-router-dom";
 import HomeScreen from "./components/home-screen";
-<<<<<<< HEAD
 import Login from "./components/users/login";
 import Register from "./components/users/register";
 import Profile from "./components/users/profile";
-=======
 import React from "react";
->>>>>>> be1c7fb3c3c1441ad91c06da826790642d5e7761
 
 function App() {
-  return (
-    <div className="container-fluid p-0">
-        <BrowserRouter>
-<<<<<<< HEAD
-            <Route path="/" exact={true}>
-                <HomeScreen/>
-            </Route>
-            <Route path="/login" exact={true}>
-                <Login/>
-            </Route>
-            <Route path="/register" exact={true}>
-                <Register/>
-            </Route>
-            <Route path="/profile" exact={true}>
-                <Profile/>
-            </Route>
-            <Route path={["/search", "/search/:location"]} exact={true}>
-=======
-            {/*<Route path="/" exact={true}>*/}
-            {/*    <HomeScreen/>*/}
-            {/*</Route>*/}
-            <Route path={["/", "/search", "/search/:location"]} exact={true}>
->>>>>>> be1c7fb3c3c1441ad91c06da826790642d5e7761
-                <SearchScreen/>
-            </Route>
-            <Route path="/details/:location/:poiID/:photoReference" exact={true}>
-                <DetailsScreen/>
-            </Route>
-        </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="container-fluid p-0">
+            <BrowserRouter>
+                {/*<Route path="/" exact={true}>*/}
+                {/*    <HomeScreen/>*/}
+                {/*</Route>*/}
+                <Route path="/login" exact={true}>
+                    <Login/>
+                </Route>
+                <Route path="/register" exact={true}>
+                    <Register/>
+                </Route>
+                <Route path="/profile" exact={true}>
+                    <Profile/>
+                </Route>
+                <Route path={["/", "/search", "/search/:location"]} exact={true}>
+                    <SearchScreen/>
+                </Route>
+                <Route path="/details/:location/:poiID/:photoReference" exact={true}>
+                    <DetailsScreen/>
+                </Route>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- travelwithlocal_react/src/components/home-screen.js
+++ travelwithlocal_react/src/components/home-screen.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import {Link, Route} from "react-router-dom"
-import "./home-screen-style.css"
+import {Link} from "react-router-dom"
 const HomeScreen = () => {
     return(
-<<<<<<< HEAD
         <div>
             <h2>Home Screen</h2>
             <Link to="/search">
@@ -28,37 +26,6 @@ const HomeScreen = () => {
             {/*</Link>
 */}
         </div>
-=======
-            // <br/>
-            // <Link to="/details">
-            //     Details
-            // </Link>
-            //
-            <div className="wbdv-sticky-top">
-                <div className="row" >
-                    <div className="col">
-                        <i className="fas fa-bars fa-2x wbdv-nav-hbg-logo"></i>
-                    </div>
-                    <div className="col-2 d-none d-lg-block wbdv-nav-title">
-                        TravelBuddy
-                    </div>
-                    <div className="col-8">
-                        <input type="text"
-                               className="form-control"
-                            // // onChange={(event) => setNewTitle(event.target.value)}
-                            //    onChange={evt => this.addCourseWithName(evt)}
-                            //    value={this.state.courseName}
-                        />
-                    </div>
-                    <div className="col-1">
-                        <Link to="/search">
-                            <a className="fas fa-search fa-2x wbdv-nav-plus-logo" role="button" ></a>
-                        </Link>
-                    </div>
-
-                </div>
-            </div>
->>>>>>> be1c7fb3c3c1441ad91c06da826790642d5e7761
     )
 }
 
