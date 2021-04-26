0b038bf90658b180694ad45a61d04923b545266e
ziyingjin94 <jin.ziyin@northeastern.edu>
2021-04-26 11:36:28 -0700

    search bar css configuration

--- travelwithlocal_react/src/components/search-screen.js
+++ travelwithlocal_react/src/components/search-screen.js
@@ -34,45 +34,38 @@ const SearchScreen = () => {
     }
 
     return(
-        <div>
-
-            <div className="wbdv-sticky-top">
-                <div className="row" >
-                    <div className="col-1">
-                        <i className="fas fa-bars fa-2x wbdv-nav-hbg-logo"></i>
+        <div className="container">
+                <div className="col row wbdv-sticky-top">
+                    <div className="d-none d-lg-block">
+                        <img src="https://i.ibb.co/sJZhzGx/47f15056e63744568e8d6704c3234446.png"  />
+                        <br/>
                     </div>
-                    <div className="col-2 d-none d-lg-block wbdv-nav-title">
-                        Travel With Locals
-                    </div>
-                    <div className="col-5">
+                    <div className="col-6 col-lg-5">
                         <input value={searchLocation}
                                onChange={(event) => {
                                    setSearchLocation(event.target.value)
                                }}
-                               className="form-control"/>
+                               className="form-control-2"/>
                     </div>
-                    <div className="col-1">
+                    <div className="col-3 col-lg-1">
                         {/*<button onClick={() => {findPOIByLocation(searchLocation)}} className="fas fa-search fa-2x wbdv-nav-plus-logo">*/}
                             <a onClick={() => {
                                 findPOIByLocation(searchLocation)
                                 findGuidesByLocation(searchLocation)
-                            }} className="fas fa-search fa-2x search-logo" role="button" ></a>
+                            }} className="fas fa-search fa-2x" role="button" ></a>
                         {/*</button>*/}
                     </div>
-                    <div className="col-2">
+                    <div className="col-lg-2 d-none d-lg-block">
                         <p>Welcome {user.getName()}</p>
                     </div>
-                    <div className="col-1 float-right">
+                    <div className="col-3 col-lg-1 float-right">
                             <Link to="/profile">
-                                <a className="btn btn-block btn-info">
-                                    Profile
-                                </a>
+                                <button type="button" className="btn btn-light float-right">Profile</button>
                             </Link>
                     </div>
                     {console.log("SearchScreen Current User: " + user.getName())}
 
                 </div>
-            </div>
 
             {/*<input value={searchLocation}*/}
             {/*       onChange={(event) => {*/}
@@ -84,46 +77,46 @@ const SearchScreen = () => {
             {/*}} className="btn btn-primary">*/}
             {/*    <a className="fas fa-search fa-2x wbdv-nav-plus-logo" role="button" ></a>*/}
             {/*</button>*/}
-            <div className="">
-                <div className="row wbdv-header-top">
-                    <div className="col-8">
-                        <ul className=" list-group">
-                            {
-                                results.results && results.results.map((poi, idx) =>{
-                                    return(
-                                        <li className="list-group-item" key={idx}>
-                                            {/*poi.place_id does not work*/}
-                                            {/*{JSON.stringify(poi.photos)}*/}
-                                            {/*react: if map, give child key!!*/}
-                                            <Link to={`/details/${poi.name}/${poi.reference}/${poi.photos[0].photo_reference}`}>
-                                                <i className={"search-result-text"}>{poi.name}</i>
-                                            </Link>
-                                        </li>
-                                    )
-                                })
-                            }
 
-                        </ul>
-                    </div>
-                    {
-                        guides.length > 0 &&
-                        <div className="col-4">
-                            <div className="row">
-                                <h3>
-                                    Recommended Guides
-                                </h3>
-                            </div>
-                            <div className="row">
-                                {
-                                    guides.map(guide =>
-                                        <GuideCard guide={guide}/> )
-                                }
-                            </div>
-                        </div>
-                    }
+            <div className="row wbdv-container-position">
+                <div className="col-8">
+                    <ul className="list-group">
+                        {
+                            results.results && results.results.map((poi, idx) =>{
+                                return(
+                                    <li className="list-group-item" key={idx}>
+                                        {/*poi.place_id does not work*/}
+                                        {/*{JSON.stringify(poi.photos)}*/}
+                                        {/*react: if map, give child key!!*/}
+                                        <Link to={`/details/${poi.name}/${poi.reference}/${poi.photos[0].photo_reference}`}>
+                                            <i className={"search-result-text"}>{poi.name}</i>
+                                        </Link>
+                                    </li>
+                                )
+                            })
+                        }
 
+                    </ul>
                 </div>
+                {
+                    guides.length > 0 &&
+                    <div className="col-4">
+                        <div className="row">
+                            <h3>
+                                Recommended Guides
+                            </h3>
+                        </div>
+                        <div className="row">
+                            {
+                                guides.map(guide =>
+                                    <GuideCard guide={guide}/> )
+                            }
+                        </div>
+                    </div>
+                }
+
             </div>
+
         </div>
     )
 }
