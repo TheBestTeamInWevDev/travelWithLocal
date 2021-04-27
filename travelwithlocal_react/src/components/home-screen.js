b75df7e5b984d57f93caa82dc7e3777e9dc68eb0
wzhu9797 <wenhuanzhu@wenhuans-MacBook-Pro.local>
2021-04-26 23:39:12 -0400

    updated responsive page

--- travelwithlocal_react/src/components/home-screen.js
+++ travelwithlocal_react/src/components/home-screen.js
@@ -22,99 +22,122 @@ const HomeScreen = () => {
                     </Link>
                 </div>
             </div>
-            <div className="d-none d-lg-block">
+            {/*<div className="d-none d-lg-block">*/}
 
-            </div>
+            {/*</div>*/}
 
             <div className="row">
                 <div className="col-sm-6">
-                    <img src="https://i.ibb.co/txgpkcs/homepage1.jpg" width="540" alt="homepage" />
+                    <img id="home_imag" src="https://i.ibb.co/txgpkcs/homepage1.jpg" width="540" alt="homepage" />
                     {/*<h4>hello</h4>*/}
                 </div>
 
-                <div className="col-sm-6 d-none d-xl-block">
-                    <h2 id="sentence">Where you off to next?</h2>
+                <div className="col-sm-6 d-block">
+                    <div className="">
+                        <h2 id="sentence">Where you off to next?</h2>
 
-                    <Link to="./search">
-                        <button
-                            type="button"
-                            className="btn btn-secondary float-right"
-                            id="get_started"
-                            to="/search/">Get Started</button>
-                    </Link>
+                        <Link to="./search">
+                            <button
+                                type="button"
+                                className="btn btn-secondary float-right"
+                                id="get_started"
+                                to="/search/">Get Started</button>
+                        </Link>
 
+                        {/*<Link to="./search">*/}
+                        {/*    <button*/}
+                        {/*        type="button"*/}
+                        {/*        className="btn btn-secondary d-block d-sm-none float-right"*/}
+                        {/*        id="get_started"*/}
+                        {/*        to="/search/">Explore</button>*/}
+                        {/*</Link>*/}
+                    </div>
+
+                    <div className="d-none d-xl-block">
                     <h2 id="looking_for">What you are looking for?</h2>
-                    <div>
-                        <Link to="./search/beach">
-                            <img  id="beach_img" src="https://i.ibb.co/pyG4Bg3/1619317771819.jpg" width="120"/>
-                            <h6 id="beach">Beach Activity</h6>
-                        </Link>
-
                     </div>
 
-                    <div>
-                        <Link to="./search/new%20york%20city">
-                            <img id="city_img" src="https://i.ibb.co/60XWSYX/1619317792809.jpg" width="160"/>
-                            <h6 id="city_tour">City Tour</h6>
-                        </Link>
-                    </div>
+                    <div className="d-none d-lg-block">
+                        <div>
+                            <Link to="./search/beach">
+                                <img  id="beach_img" src="https://i.ibb.co/pyG4Bg3/1619317771819.jpg" width="120"/>
+                                <h6 id="beach">Beach Activity</h6>
+                            </Link>
 
-                    <div>
-                        <Link to="./search/hiking">
-                            <img id="hiking_img" src="https://i.ibb.co/5Ykk722/1619317798415.jpg" width="160"/>
-                            <h6 id="hiking">Hiking Trips</h6>
-                        </Link>
-                    </div>
+                        </div>
 
-                    <div>
-                        <Link to="./search/food">
-                            <img id="foodie_img"src="https://i.ibb.co/1r5WBkX/1619317803673.jpg" width="160"/>
-                            <h6 id="foodie">Foodie Explore</h6>
-                        </Link>
+                        <div>
+                            <Link to="./search/new%20york%20city">
+                                <img id="city_img" src="https://i.ibb.co/60XWSYX/1619317792809.jpg" width="160"/>
+                                <h6 id="city_tour">City Tour</h6>
+                            </Link>
+                        </div>
+
+                        <div>
+                            <Link to="./search/hiking">
+                                <img id="hiking_img" src="https://i.ibb.co/5Ykk722/1619317798415.jpg" width="160"/>
+                                <h6 id="hiking">Hiking Trips</h6>
+                            </Link>
+                        </div>
+
+                        <div>
+                            <Link to="./search/food">
+                                <img id="foodie_img"src="https://i.ibb.co/1r5WBkX/1619317803673.jpg" width="160"/>
+                                <h6 id="foodie">Foodie Explore</h6>
+                            </Link>
+                        </div>
                     </div>
                 </div>
 
             </div>
             <br/>
 
-            {/*<div className="row">*/}
-                <div className="col vac_text">
+            <Link to="/search">
+                <button className="btn btn-secondary mid_button">More</button>
+            </Link>
+
+            <br/>
+
+            <div className="row">
+                <div className="col vac_text d-none d-lg-block">
                     <i id="trip_icon"className="fas fa-route"></i>
                     <i id="fly_icon" className="fas fa-plane-departure"></i>
-                    <h1 id="vaca_text">Vacation mood on...</h1>
+                    <h6 id="vaca_text">Vacation mood on...</h6>
                 </div>
-            {/*</div>*/}
+            </div>
             <br/>
 
 
-            <div className="cla_gal_1">
-                <Link to = "./search/yellow%20stone">
-                    <img id="gallery_1" className="image" src="https://i.ibb.co/xqM5s6K/IMG-20190609-120959.jpg" width="200" height="200"/>
-                </Link>
-                {/*<div className="overlay">*/}
-                {/*    <div className="text">John Doe</div>*/}
-                {/*</div>*/}
-            </div>
 
-            <div className="cla_gal_2">
-                <Link to = "./search/zoom">
-                    <img id="gallery_2" src="https://i.ibb.co/mC04th6/IMG-20190609-121623.jpg" width="200" height="200"/>
-                </Link>
-                {/*<div className="desc">Add a description of the image here</div>*/}
-            </div>
+            <div className="d-none d-xl-block">
+                <div className="cla_gal_1">
+                    <Link to = "./search/yellow%20stone">
+                        <img id="gallery_1" className="image" src="https://i.ibb.co/xqM5s6K/IMG-20190609-120959.jpg" width="200" height="200"/>
+                    </Link>
 
-            <div className="cla_gal_3">
-                <Link to = "./search/park">
-                    <img id="gallery_3" src="https://i.ibb.co/X3RFgfX/IMG-20190609-124957.jpg" width="200" height="200"/>
-                </Link>
-                {/*<div className="desc">Add a description of the image here</div>*/}
-            </div>
+                </div>
 
-            <div className="cla_gal_4">
-                <Link to = "./search/lake">
-                    <img id="gallery_4" src="https://i.ibb.co/d7r504F/IMG-8834.jpg"  width="200" height="200" />
-                </Link>
-                {/*<div className="desc">Add a description of the image here</div>*/}
+                <div className="cla_gal_2">
+                    <Link to = "./search/zoom">
+                        <img id="gallery_2" src="https://i.ibb.co/mC04th6/IMG-20190609-121623.jpg" width="200" height="200"/>
+                    </Link>
+                    {/*<div className="desc">Add a description of the image here</div>*/}
+                </div>
+
+                <div className="cla_gal_3">
+                    <Link to = "./search/park">
+                        <img id="gallery_3" src="https://i.ibb.co/X3RFgfX/IMG-20190609-124957.jpg" width="200" height="200"/>
+                    </Link>
+                    {/*<div className="desc">Add a description of the image here</div>*/}
+                </div>
+
+                <div className="cla_gal_4">
+                    <Link to = "./search/lake">
+                        <img id="gallery_4" src="https://i.ibb.co/d7r504F/IMG-8834.jpg"  width="200" height="200" />
+                    </Link>
+                    {/*<div className="desc">Add a description of the image here</div>*/}
+                </div>
+
             </div>
 
             {/*<div className="cla_gal_5">*/}
@@ -145,13 +168,12 @@ const HomeScreen = () => {
             {/*    /!*<div className="desc">Add a description of the image here</div>*!/*/}
             {/*</div>*/}
 
-            <div className="explore">
+            <div className="explore d-none d-xl-block">
                 <p>Explore More</p>
                 <Link to="/search">
                     <button className="btn btn-secondary">More</button>
                 </Link>
             </div>
-
 
         </div>
     )
